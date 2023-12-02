4f1641b8462a2df96d215e93eb9cd214bbe85bb2
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-02 18:00:32 -0300

    quinto projeto

---
+++ BEE1004/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine() ?? "0");
+        int B = int.Parse(Console.ReadLine() ?? "0");
+
+        int PROD = A * B;
+
+        Console.WriteLine("PROD = {0}", PROD);
+
+    }
+
+}
