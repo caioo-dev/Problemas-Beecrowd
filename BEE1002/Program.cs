3c4a0157cc5e7fdad2681ed8984df72b2218e57b
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-02 17:56:38 -0300

    terceiro projeto

---
+++ BEE1002/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double raio = double.Parse(Console.ReadLine() ?? "0");
+        double N = 3.14159;
+
+        double A = N * Math.Pow(raio, 2.0);
+
+        Console.WriteLine("A={0}", A.ToString("F4"));
+
+    }
+
+}
