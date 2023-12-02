0fa1c3d946563816794e719a1a2373659934d7d6
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-02 17:58:55 -0300

    quarto projeto

---
+++ BEE1003/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int A = int.Parse(Console.ReadLine() ?? "0");
+        int B = int.Parse(Console.ReadLine() ?? "0");
+
+        int soma = A + B;
+
+        Console.WriteLine("SOMA = {0}", soma);
+    }
+
+}
