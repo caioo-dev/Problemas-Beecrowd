2bfab74d65e2c0e4d3d0dd897a20ef77044470e6
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-04 18:34:52 -0300

    decimo

---
+++ BEE1009/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string nome = Console.ReadLine();
+        double salario = Convert.ToDouble(Console.ReadLine());
+        double totalVendas = Convert.ToDouble(Console.ReadLine());
+
+        salario += totalVendas * 0.15;
+
+        Console.WriteLine("TOTAL = R$ " + salario.ToString("F2"));
+    }   
+
+}
