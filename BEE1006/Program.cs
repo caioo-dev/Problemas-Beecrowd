d4cd839cb5fddc7833873e23c0ebb853b32055d2
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-03 22:06:50 -0300

    setimo projeto

---
+++ BEE1006/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double A = double.Parse(Console.ReadLine() ?? "0");
+        double B = double.Parse(Console.ReadLine() ?? "0");
+        double C = double.Parse(Console.ReadLine() ?? "0");
+
+        double MEDIA = (A * 2.0 + B * 3.0 + C * 5.0) / 10.0;
+
+        Console.WriteLine("MEDIA = {0}", MEDIA.ToString("F1"));
+    }
+
+}
