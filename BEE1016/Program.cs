8c131577d36787545c3c03ec9ba9492193a63fa9
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-04 18:47:22 -0300

    BEE1016

---
+++ BEE1016/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int distanciaKm = Convert.ToInt32(Console.ReadLine());
+
+        int tempoMinutos = distanciaKm * 2;
+
+        Console.WriteLine(tempoMinutos + " minutos");
+    }
+
+}
