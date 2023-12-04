5a9c69e9ce0c44060ffa2a90caee7ecc06ac04f4
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-03 22:11:23 -0300

    nono projeto

---
+++ BEE1008/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine() ?? "0");
+        int workedHours = int.Parse(Console.ReadLine() ?? "0");
+        double dolarPerHour = double.Parse(Console.ReadLine() ?? "0");
+
+        double salary = workedHours * dolarPerHour;
+
+        Console.WriteLine("NUMBER = " + number);
+        Console.WriteLine("SALARY = U$ " + salary.ToString("F2"));
+    }
+
+}
