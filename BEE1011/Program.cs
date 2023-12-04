2a37fce0ef9ce56efda99ff34a10961eb258d6f0
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-04 18:39:59 -0300

    BEE1011

---
+++ BEE1011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int raio = Convert.ToInt32(Console.ReadLine());
+
+        double pi = 3.14159;
+
+        double volume = (4.0 / 3.0) * pi * Math.Pow(raio, 3.0);
+
+        Console.WriteLine("VOLUME = " + volume.ToString("F3"));
+    }
+
+}
