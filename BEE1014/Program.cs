5da8348666dd8cf543a7ae68d0d7b13e0b4eac71
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-04 18:44:49 -0300

    BEE1014

---
+++ BEE1014/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int distanciaKm = Convert.ToInt32(Console.ReadLine());
+        double totalCombustivel = Convert.ToDouble(Console.ReadLine());
+
+        double kmPorLitros = distanciaKm / totalCombustivel;
+
+        Console.WriteLine(kmPorLitros.ToString("F3") + " km/l");
+    }
+
+}
