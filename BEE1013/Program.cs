22594a31aae6294f5cf6669350e3b26f44a9aa3b
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-04 18:43:00 -0300

    BEE1013

---
+++ BEE1013/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string[] numeros = Console.ReadLine().Split(' ');
+        int A = Convert.ToInt32(numeros[0]);
+        int B = Convert.ToInt32(numeros[1]);
+        int C = Convert.ToInt32(numeros[2]);
+
+        int MaiorAB = (A + B + Math.Abs(A - B)) / 2;
+
+        if (MaiorAB > C)
+        {
+            Console.WriteLine(MaiorAB + " eh o maior");
+        }
+        else
+        {
+            Console.WriteLine(C + " eh o maior");
+        }
+    }
+
+}
