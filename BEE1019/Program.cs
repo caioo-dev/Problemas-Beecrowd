73bae84523acd0463dd79aeb4d9cc29e66777cf8
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-05 22:31:50 -0300

    BEE1019

---
+++ BEE1019/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int segundos = Convert.ToInt32(Console.ReadLine());
+
+        int horas = segundos / 3600;
+        int minutos = (segundos % 3600) / 60;
+        segundos = segundos % 60;
+
+        Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+    }
+
+}
