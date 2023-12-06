fffe2b7ab264ee5aa90ef288b711c91e21350e93
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-05 22:27:13 -0300

    BEE1017

---
+++ BEE1017/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int tempoDeViagem = Convert.ToInt32(Console.ReadLine());
+        int velocidadeMedia = Convert.ToInt32(Console.ReadLine());
+
+        double qntLitros = (double)tempoDeViagem * velocidadeMedia / 12;
+
+        Console.WriteLine(qntLitros.ToString("F3"));
+    }
+
+}
