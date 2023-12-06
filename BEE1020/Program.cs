6d9a87ad72dda67633c2afa05af8888b7ad97822
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-05 22:39:10 -0300

    BEE1020

---
+++ BEE1020/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int idadeEmDias = Convert.ToInt32(Console.ReadLine());
+
+        int ano = idadeEmDias / 365;
+        int resto = idadeEmDias % 365;
+        int mes = resto / 30;
+        int dia = resto % 30;
+
+        Console.WriteLine(ano + " ano(s)");
+        Console.WriteLine(mes + " mes(es)");
+        Console.WriteLine(dia + " dia(s)");
+    }
+
+}
