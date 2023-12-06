d9e765db8ca19a8f52557d28c88b3814a2ef9cd3
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-05 22:29:03 -0300

    BEE1018

---
+++ BEE1018/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int[] notas = new int[7] { 100, 50, 20, 10, 5, 2, 1 };
+
+        int valor = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(valor);
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            Console.WriteLine(valor / notas[i] + " nota(s) de R$ " + notas[i] + ",00");
+            valor = (valor % notas[i]);
+        }
+    }
+
+}
