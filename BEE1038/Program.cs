31206ef62ada430f9fba8c369943cb8e16cd9b65
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-07 00:18:48 -0300

    BEE1038

---
+++ BEE1038/Program.cs
@@ -0,0 +1,33 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+            string[] valores = Console.ReadLine().Split(' ');
+            int codigo = Convert.ToInt32(valores[0]);
+            int quantidade = Convert.ToInt32(valores[1]);
+            double total = 0.0;
+
+            switch (codigo)
+            {
+                case 1:
+                    total = quantidade * 4.00;
+                    break;
+                case 2:
+                    total = quantidade * 4.50;
+                    break;
+                case 3:
+                    total = quantidade * 5.00;
+                    break;
+                case 4:
+                    total = quantidade * 2.00;
+                    break;
+                case 5:
+                    total = quantidade * 1.50;
+                    break;
+            }
+
+            Console.WriteLine("Total: R$ " + total.ToString("F2"));
+    }
+
+}
