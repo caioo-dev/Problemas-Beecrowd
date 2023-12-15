02f6b1b3fe815eac0d9c18f5268a591a4a30e5b0
Caio Andrade <caio.andrade2442@gmail.com>
2023-12-14 23:35:38 -0300

    BEE1044

---
+++ BEE1044/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string[] valores = Console.ReadLine().Split(' ');
+        int n1 = Convert.ToInt32(valores[0]);
+        int n2 = Convert.ToInt32(valores[1]);
+
+        if (n1 % n2 == 0 || n2 % n1 == 0)
+        {
+            Console.WriteLine("Sao Multiplos");
+        }
+        else
+        {
+            Console.WriteLine("Nao sao Multiplos");
+        }
+    }
+}
