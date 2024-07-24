3ab2af8d209a4eed5f34b09cd92fecf7ce0f3b18
Caio Andrade <caio.andrade2442@gmail.com>
2024-07-24 16:30:44 -0300

    BEE1046

---
+++ BEE1046/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+
+        string[] horas = Console.ReadLine().Split(' ');
+
+        int horaInicio = Convert.ToInt32(horas[0]);
+        int horaFinal = Convert.ToInt32(horas[1]);
+        int duracao = 0;
+
+        if (horaInicio >= horaFinal)
+        {
+            duracao = (24 + horaFinal) - horaInicio;
+        }
+        else
+        {
+            duracao = (horaFinal - horaInicio);
+        }
+
+        Console.WriteLine(duracao);
+    }
+}
